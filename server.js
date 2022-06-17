f5a3485cc31860aaad4315f7a3055ffbe712f997
leliovictor <leliov.vieira@gmail.com>
2022-06-17 04:21:54 -0300

    Added post request

--- server.js
+++ server.js
@@ -3,11 +3,15 @@ import cors from 'cors';
 
 const server = express();
 server.use(cors());
+server.use(express.json()); //ler mais sobre isso ainda;
 
+let users = [];
 
+server.post("/sign-up",(req,res)=>{
+    users.push(req.body);
+    
+    res.send("OK");
 
-
-
-
+});
 
 server.listen(5000);
