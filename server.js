d81c5a00f0c7be76e298337b788e6fe6c86dd803
leliovictor <leliov.vieira@gmail.com>
2022-06-18 13:34:43 -0300

    Added validation of sign-up - username and imageURL

--- server.js
+++ server.js
@@ -8,24 +8,31 @@ server.use(express.json());
 const users = [];
 const tweets = [];
 
-server.post("/sign-up", (req, res) => {
-  users.push(req.body);
+function isImage(url) {
+  return /\.(jpg|jpeg|png|webp|avif|gif|svg)$/.test(url);
+}
 
-  console.log(users);
+server.post("/sign-up", (req, res) => {
+  const { username, avatar } = req.body;
+  
+  if (username === "" || avatar === "")
+  res.status(400).send("Todos os campos são obrigatórios!");
+  
+  if (!isImage(avatar)) res.status(400).send("Foto com formato inválido");
+
+  users.push(req.body);
   res.send("OK");
 });
 
 server.get("/tweets", (req, res) => {
-  const lastTweets = tweets.slice(tweets.length-10, tweets.length).reverse(); //Agiliza?
+  const lastTweets = tweets.slice(-10, tweets.length).reverse();
   res.send(lastTweets);
 });
 
 server.post("/tweets", (req, res) => {
   const { username, tweet } = req.body;
-
   const user = users.find((obj) => username === obj.username);
-
-  tweets.push({...req.body, avatar:user.avatar});
+  tweets.push({ ...req.body, avatar: user.avatar });
 
   res.send("OK");
 });
