46807ce697298dc5c28604e9e2c653bed67c968b
leliovictor <leliov.vieira@gmail.com>
2022-06-18 12:26:44 -0300

    Added post/tweets and get/tweets in API

--- server.js
+++ server.js
@@ -1,17 +1,33 @@
-import express from 'express';
-import cors from 'cors';
+import express from "express";
+import cors from "cors";
 
 const server = express();
 server.use(cors());
-server.use(express.json()); //ler mais sobre isso ainda;
+server.use(express.json());
 
-let users = [];
+const users = [];
+const tweets = [];
 
-server.post("/sign-up",(req,res)=>{
-    users.push(req.body);
-    
-    res.send("OK");
+server.post("/sign-up", (req, res) => {
+  users.push(req.body);
 
+  console.log(users);
+  res.send("OK");
+});
+
+server.get("/tweets", (req, res) => {
+  const lastTweets = tweets.slice(tweets.length-10, tweets.length).reverse(); //Agiliza?
+  res.send(lastTweets);
+});
+
+server.post("/tweets", (req, res) => {
+  const { username, tweet } = req.body;
+
+  const user = users.find((obj) => username === obj.username);
+
+  tweets.push({...req.body, avatar:user.avatar});
+
+  res.send("OK");
 });
 
 server.listen(5000);
