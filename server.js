eb89947be41d95c95d030c5dfdce9d163a1f1871
leliovictor <leliov.vieira@gmail.com>
2022-06-20 14:22:21 -0300

    Added validation of last post - user and tweet empty blocked - and reverse list array from a single person tweets - show last to first now -

--- server.js
+++ server.js
@@ -5,28 +5,8 @@ const server = express();
 server.use(cors());
 server.use(express.json());
 
-const users = []; //juntar os dois e ja adicionar o avatar ao tweet no post para facilitar?
+const users = [];
 const tweets = [];
-
-//Function below just to population tweets to test, erase after done!
-//!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!
-
-function populationTweets(n) {
-  let count = 0;
-  for (let i = 0; i < n; i++) {
-    const y = i % 2 === 0 ? i : i - 1;
-    const obj = {
-      username: `Teste-${y}`,
-      avatar:
-        "https://super.abril.com.br/wp-content/uploads/2020/09/04-09_gato_SITE.jpg?quality=70&strip=info",
-      tweet: `Tweet ${i + 1}`,
-    };
-    tweets.push(obj);
-  }
-}
-
-populationTweets(15); //ERASE AFTER DONE !
-console.log(tweets); //ERASE AFTER DONE !
 
 function isImage(url) {
   return /\.(jpg|jpeg|png|webp|avif|gif|svg)$/.test(url);
@@ -46,13 +26,11 @@ server.post("/sign-up", (req, res) => {
 
   users.push(req.body);
 
-  console.log(users);
-
   res.status(201).send("OK");
 });
 
 server.get("/tweets", (req, res) => {
-  if (isNaN(req.query.page) || req.query.page < 0)
+  if (isNaN(req.query.page) || req.query.page <= 0)
     res.status(400).send("Informe uma página válida!");
 
   const numbersOfTweetsPerPage = 10;
@@ -71,14 +49,17 @@ server.get("/tweets", (req, res) => {
 
 server.get("/tweets/:person", (req, res) => {
   const person = req.params.person;
-
   const personTweets = tweets.filter((obj) => person === obj.username);
+  personTweets.reverse();
   res.send(personTweets);
 });
 
 server.post("/tweets", (req, res) => {
   const { tweet } = req.body;
   const { user } = req.headers;
+
+  if (user === "" || tweet === "")
+    return res.status(400).send("Todos os campos são obrigatórios!");
 
   const username = users.find((obj) => user === obj.username);
   tweets.push({ ...username, tweet });
